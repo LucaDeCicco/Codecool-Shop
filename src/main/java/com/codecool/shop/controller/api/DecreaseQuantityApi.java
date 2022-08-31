faff4b077a55a1b646e20f2a1d5ba23e7c6c186b
LucaDeCicco <luca14.decicco@yahoo.com>
2022-08-31 13:53:41 +0300

    edit cart quantity

---
+++ src/main/java/com/codecool/shop/controller/api/DecreaseQuantityApi.java
@@ -0,0 +1,50 @@
+package com.codecool.shop.controller.api;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Item;
+import com.codecool.shop.model.Product;
+import com.google.gson.JsonObject;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+
+@WebServlet(name = "DecreaseQuantityServlet", urlPatterns = {"/api/decreaseQuantity"}, loadOnStartup = 1)
+public class DecreaseQuantityApi extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        CartDaoMem cartDataStore = CartDaoMem.getInstance();
+
+        List<Item> cartProducts = cartDataStore.getAll();
+
+        StringBuffer buffer = new StringBuffer();
+        String line;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        String bufferID = buffer.substring(7, 8);
+        String bufferQuantity = buffer.substring(22,23);
+
+
+        for (Item item : cartProducts) {
+            if (item.getId()==Integer.parseInt(bufferID)){
+                item.setQuantity(Integer.parseInt(bufferQuantity));
+            }
+        }
+
+    }
+}
--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -29,7 +29,77 @@ const deleteFromCartBtnHandler = () => {
 }
 
 
+const increaseQuantityBtnHandler = () => {
+    let increaseButtons = document.querySelectorAll(".increaseQuantity")
+    for (const increaseButton of increaseButtons) {
+        increaseButton.addEventListener("click", async () => {
+            let prodId = increaseButton.getAttribute("data-id")
+            let quantities = document.querySelectorAll(".quantity")
+            for (const quantity of quantities) {
+                let newQuantity = (parseInt(quantity.innerHTML) + 1).toString()
+                if (quantity.getAttribute("data-id") === prodId) {
+                    const dataToBePosted = {
+                        id: prodId,
+                        quantity: newQuantity
+                    };
+                    const response = await fetch('/api/increaseQuantity', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(dataToBePosted)
+                    });
+                    quantity.innerHTML = newQuantity;
+                }
+            }
+        })
+    }
+}
 
+const decreaseQuantityBtnHandler = () => {
+    let decreaseButtons = document.querySelectorAll(".decreaseQuantity")
+    for (const decreaseButton of decreaseButtons) {
+        decreaseButton.addEventListener("click", async () => {
+            let prodId = decreaseButton.getAttribute("data-id")
+            let quantities = document.querySelectorAll(".quantity")
+            for (const quantity of quantities) {
+                if (quantity.getAttribute("data-id") === prodId) {
+                    let newQuantity = (parseInt(quantity.innerHTML) - 1).toString()
+                    const dataToBePosted = {
+                        id: prodId,
+                        quantity: newQuantity
+                    };
+
+                    if (parseInt(newQuantity)===0){
+                        const response = await fetch("/api/deleteFromCart", {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(dataToBePosted)
+                        });
+                        let cards = document.querySelectorAll('.row>div>div')
+                        for (const card of cards) {
+                            if (card.getAttribute("data-id")===prodId){
+                                card.innerHTML=""
+                            }
+                        }
+                    }
+                    else {
+                        const response = await fetch("/api/decreaseQuantity", {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(dataToBePosted)
+                        });
+                        quantity.innerHTML = (parseInt(quantity.innerHTML) - 1).toString();
+                    }
+                }
+            }
+        })
+    }
+}
 
 
 
@@ -41,6 +111,8 @@ const deleteFromCartBtnHandler = () => {
 
 const init = () => {
     deleteFromCartBtnHandler();
+    increaseQuantityBtnHandler();
+    decreaseQuantityBtnHandler();
 }
 
 init();
